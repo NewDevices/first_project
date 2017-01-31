7c251374c5d8bedde1ec0fa274f29df410e02676
Hendrikto <hendrik.to@gmail.com>
2017-01-31 15:52:24 +0100

    Add a function to translate analog values to key codes.

--- project/project.ino
+++ project/project.ino
@@ -4,6 +4,9 @@
 #define BASE_CODE1 5313876
 #define BASE_CODE2 5326164
 #define BIT_LENGTH 24
+#define KEY_COUNT 5
+
+const int keyLimits[] = {50, 190, 380, 555, 790, 1024};
 
 LiquidCrystal lcd = LiquidCrystal(8, 9, 4, 5, 6, 7);
 RCSwitch rcSwitch = RCSwitch();
@@ -19,6 +22,28 @@ long getRcCode(int receiver, bool setOn) {
 		case 2: code += BASE_CODE2; break;
 	}
 	return code;
+}
+
+/**
+ * Return they key code that corresponds to a given value.
+ *
+ * KeyCode | Key
+ * --------+-----------------------
+ *       5 | no key
+ *       4 | select
+ *       3 | left
+ *       2 | down
+ *       1 | up
+ *       0 | right
+ *      -1 | Could not resolve key.
+ */
+int getKeyCode(int value) {
+	for (int i = 0; i <= KEY_COUNT; i++) {
+		if (value < keyLimits[i]) {
+			return i;
+		}
+	}
+	return -1;
 }
 
 void setup() {
