6794beca1bf7c0162221d9973b50de016df1b952
Hendrikto <hendrik.to@gmail.com>
2017-01-31 15:20:20 +0100

    Add a project wireframe.

---
+++ project/project.ino
@@ -0,0 +1,15 @@
+#include <RCSwitch.h>
+#include <LiquidCrystal.h>
+
+#define BASE_CODE1 5313876
+#define BASE_CODE2 5326164
+#define BIT_LENGTH 24
+
+LiquidCrystal lcd = LiquidCrystal(8, 9, 4, 5, 6, 7);
+RCSwitch rcSwitch = RCSwitch();
+
+void setup() {
+}
+
+void loop() {
+}
