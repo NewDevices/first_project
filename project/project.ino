516fb58e9bc3aaa1b9b406c07ed405dea44034ad
Hendrikto <hendrik.to@gmail.com>
2017-01-31 15:20:49 +0100

    Add a function for retrieving RC codes.

--- project/project.ino
+++ project/project.ino
@@ -8,6 +8,19 @@
 LiquidCrystal lcd = LiquidCrystal(8, 9, 4, 5, 6, 7);
 RCSwitch rcSwitch = RCSwitch();
 
+/**
+ * Get the RC code for a given receiver corresponding to whether you want to set
+ * it to on or off.
+ */
+long getRcCode(int receiver, bool setOn) {
+	long code = setOn ? 1 : 0;
+	switch (receiver) {
+		case 1: code += BASE_CODE1; break;
+		case 2: code += BASE_CODE2; break;
+	}
+	return code;
+}
+
 void setup() {
 }
 
